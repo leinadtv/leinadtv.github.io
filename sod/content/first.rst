dcdd5dc0f5d6395f7b56f4dd6f130ad5cf6729e2
Daniel Valent <danielvalent@gmail.com>
2019-02-12 20:05:52 +0000

    init

---
+++ sod/content/first.rst
@@ -0,0 +1,8 @@
+An article
+##########
+
+:date: 2019-02-12
+:author: Dano
+:summary: Article summary. Perex of sorts perhaps
+
+Article content starts here.
---
+++ sod/pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Dano'
+SITENAME = 'Sight of Dano'
+SITEURL = 'https://github.com/leinadtv'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+LOAD_CONTENT_CACHE = False
+
+THEME = 'm.css/pelican-theme'
+THEME_STATIC_DIR = 'static'
+DIRECT_TEMPLATES = ['index']
+
+M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700,700i%7CSource+Code+Pro:400,400i,600',
+               '/static/m-light.css']
+M_THEME_COLOR = '#cb4b16'
+
+PLUGIN_PATHS = ['m.css/pelican-plugins']
+PLUGINS = ['m.htmlsanity']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
